eb1ea4f8d10c43efa039f1e8d1f1a899b2825c16
AnastassiaEE <martsenkoanastassia56@gmail.com>
2025-06-21 19:35:35 +0300

    Add player health bar.

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,4 +1,6 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -6,24 +8,44 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField] private int maxHealth;
     private int health;
 
+    [SerializeField] Slider healthBar;
+    [SerializeField] TextMeshProUGUI healthText;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         health = maxHealth;
+        healthBar.maxValue = maxHealth;
+        UpdateUI();
+        Time.timeScale = 1f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
+
+    private void UpdateUI()
+    {
+        healthBar.value = health;
+        healthText.text = health.ToString();
+    }
+
 
     public void TakeDamage(int damage)
     {
-        int realDamage = Mathf.Min(damage, health);
-        health -= realDamage;
+        health -= damage;
+        health = Mathf.Clamp(health, 0, maxHealth);
+        UpdateUI();
         if (health <= 0) PassAway();
     }
+
+    [NaughtyAttributes.Button]
+    private void Damage()
+    {
+        TakeDamage(10);
+    }
+
 
     private void PassAway()
     {
